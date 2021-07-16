f269b0f06602fc664a2b0ed5629f2d0ec8d6798c
Danila Derzhavin <derzhavin.danila@icloud.com>
2021-07-16 14:58:36 +0400

    changed OrderForm component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-import OrderForm from './Form'
+import OrderForm from "./OrderForm";
 
 function App() {
   return (
--- src/OrderForm.tsx
+++ src/OrderForm.tsx
@@ -1,69 +1,73 @@
-import { AppBar, CssBaseline, makeStyles, Paper, Theme, Toolbar, Typography } from '@material-ui/core'
-import React from 'react'
-import Form from './Form'
+import React from "react";
+import {
+    AppBar,
+    createStyles,
+    CssBaseline,
+    Grid,
+    makeStyles,
+    Paper,
+    Theme,
+    Toolbar,
+    Typography, withStyles
+} from "@material-ui/core";
+import './Form';
+import Form from "./Form";
 
-const useStyles = makeStyles((theme: Theme) => ({
+const useStyles = makeStyles((theme?: any) => ({
     appBar: {
-      position: 'relative',
+        position: 'relative'
     },
     layout: {
-      width: 'auto',
-      marginLeft: theme.spacing(2),
-      marginRight: theme.spacing(2),
-      [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
-        width: 600,
-        marginLeft: 'auto',
-        marginRight: 'auto',
-      },
+        width: 'auto',
+        marginLeft: theme.spacing(2),
+        marginRight: theme.spacing(2),
+        [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
+            width: 600,
+            marginLeft: 'auto',
+            marginRight: 'auto'
+        },
     },
     paper: {
-      marginTop: theme.spacing(3),
-      marginBottom: theme.spacing(3),
-      padding: theme.spacing(2),
-      [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
-        marginTop: theme.spacing(6),
-        marginBottom: theme.spacing(6),
-        padding: theme.spacing(3),
-      },
-    },
-    stepper: {
-      padding: theme.spacing(3, 0, 5),
+        marginTop: theme.spacing(3),
+        marginBottom: theme.spacing(3),
+        padding: theme.spacing(2),
+        [theme.breakpoints.up(600 + theme.spacing(3) + 2)]: {
+            marginTop: theme.spacing(6),
+            marginBottom: theme.spacing(6),
+            padding: theme.spacing(3),
+        },
     },
     buttons: {
-      display: 'flex',
-      justifyContent: 'flex-end',
+        display: 'flex',
+        justifyContent: 'flexEnd',
     },
     button: {
-      marginTop: theme.spacing(3),
-      marginLeft: theme.spacing(1),
-    },
-  }));
+        marginTop: theme.spacing(3),
+        marginLeft: theme.spacing(1)
+    }
+}));
 
 export default function OrderForm() {
     const classes = useStyles();
 
-    return (
+    return(
         <React.Fragment>
             <CssBaseline/>
-            <AppBar position='absolute' color='default' className={classes.appBar}>
+            <AppBar position={"absolute"} color={"default"} className={classes.appBar}>
                 <Toolbar>
-                    <Typography variant='h6' color='inherit' noWrap>
+                    <Typography variant={"h6"} color='inherit' noWrap>
                         Order manager
                     </Typography>
                 </Toolbar>
             </AppBar>
             <main className={classes.layout}>
                 <Paper className={classes.paper}>
-                    <Typography component='h1' variant='h4' align='center'>
+                    <Typography>
                         Форма заказа
                     </Typography>
-                    <React.Fragment>
-                        <div className={classes.buttons}>
-                            <Form/>
-                        </div>
-                    </React.Fragment>
+                    <Form/>
                 </Paper>
-            </main>
+            </main >
         </React.Fragment>
     )
 }
