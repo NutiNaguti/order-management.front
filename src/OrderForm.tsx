50b36f442eed0d8d4da7879a8c7554e1fc43648c
Danila Derzhavin <derzhavin.danila@icloud.com>
2021-07-17 13:43:43 +0400

    changed OrderForm.tsx component

--- src/OrderForm.tsx
+++ src/OrderForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, {ChangeEvent, useState} from "react";
 import {
     AppBar,
     CssBaseline,
@@ -47,14 +47,48 @@ const useStyles = makeStyles((theme: Theme) => ({
 
 export default function OrderForm() {
     const classes = useStyles();
-    const [data, setData] = useState({
-        firstNameValue: "", secondNameValue: "", shortDescriptionValue: "", priceValue: 0
-    });
 
-    const getData = () => {
-        // TODO поменять вызов на POST запрос
-        return axios('http://localhost:5000/api/HealthCheck');
+    const [firstName, setFirstName] = useState('');
+    const [secondName, setSecondName] = useState('');
+    const [shortDescription, setShortDescription] = useState('');
+    const [price, setPrice] = useState(0.0);
+
+    const sendRequest = async () => {
+        const response = await axios({
+            method: "POST",
+            url: "http://localhost:5000/api/OrderManagement",
+            data: {
+                fio: `${firstName} ${secondName}`,
+                description: shortDescription,
+                price: price
+            }
+        });
+        console.log(response)
     }
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        switch (event.target.name) {
+            case 'firstName':
+                setFirstName(event.target.value)
+                console.log(firstName)
+                break;
+
+            case 'secondName':
+                setSecondName(event.target.value)
+                console.log(secondName)
+                break;
+
+            case 'shortDescription':
+                setShortDescription(event.target.value)
+                console.log(shortDescription)
+                break;
+
+            case 'price':
+                setPrice(Math.abs(parseFloat(event.target.value)))
+                console.log(price)
+                break;
+        }
+    };
 
     return(
         <React.Fragment>
@@ -73,19 +107,41 @@ export default function OrderForm() {
                     </Typography>
                     <Grid container spacing={3}>
                         <Grid item xs={12} sm={6}>
-                            <TextField required id="firstName" name="firstName" label="First name" fullWidth/>
+                            <TextField value={firstName}
+                                       onChange={handleChange}
+                                       required id="firstName"
+                                       name="firstName"
+                                       label="First name"
+                                       fullWidth/>
                         </Grid>
                         <Grid item xs={12} sm={6}>
-                            <TextField required id="lastName" name="lastName" label="Last name" fullWidth/>
+                            <TextField value={secondName}
+                                       onChange={handleChange}
+                                       required id="secondName"
+                                       name="secondName"
+                                       label="Second name"
+                                       fullWidth/>
                         </Grid>
                         <Grid item xs={12} sm={6}>
-                            <TextField required id="shortDescription" name="shortDescription" label="Short description" fullWidth/>
+                            <TextField value={shortDescription}
+                                       onChange={handleChange}
+                                       required id="shortDescription"
+                                       name="shortDescription"
+                                       label="Short description"
+                                       fullWidth/>
                         </Grid>
                         <Grid item xs={12} sm={6}>
-                            <TextField required id="price" name="price" label="Price" fullWidth/>
+                            <TextField value={price}
+                                       onChange={handleChange}
+                                       required
+                                       type="number"
+                                       id="price"
+                                       name="price"
+                                       label="Price"
+                                       fullWidth/>
                         </Grid>
                     </Grid>
-                    <Button onClick={getData} className={classes.button}>
+                    <Button onClick={sendRequest} className={classes.button}>
                         Создать заказ
                     </Button>
                 </Paper>
